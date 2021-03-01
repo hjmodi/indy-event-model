a7461f227e116c1ed3de2bf0c54892f9c3b91ec0
Wenjie Guo <wguo@redhat.com>
2021-03-01 10:37:53 +0800

    Add the version and implement Externalizable for event model

--- src/main/java/org/commonjava/event/common/IndyEvent.java
+++ src/main/java/org/commonjava/event/common/IndyEvent.java
@@ -1,11 +1,15 @@
 package org.commonjava.event.common;
 
-import org.commonjava.event.common.EventMetadata;
-
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.UUID;
 
-public class IndyEvent
+public class IndyEvent implements Externalizable
 {
+
+    private static final int VERSION = 1;
 
     private UUID eventID;
 
@@ -26,4 +30,25 @@ public class IndyEvent
         this.eventMetadata = eventMetadata;
     }
 
+    public void writeExternal( ObjectOutput out ) throws IOException
+    {
+        out.writeObject( Integer.toString( VERSION ) );
+        out.writeObject( eventID );
+        out.writeObject( eventMetadata );
+    }
+
+    public void readExternal( ObjectInput in ) throws IOException, ClassNotFoundException
+    {
+        int version = Integer.parseInt( ( String )in.readObject() );
+        if ( version > VERSION )
+        {
+            throw new IOException(
+                            "Cannot deserialize. This class is of an older version: " + VERSION +
+                                            " vs. the version read from the data stream: " + version + "." );
+        }
+
+        this.eventID = (UUID) in.readObject();
+        this.eventMetadata = (EventMetadata) in.readObject();
+
+    }
 }
--- src/main/java/org/commonjava/event/file/FileEvent.java
+++ src/main/java/org/commonjava/event/file/FileEvent.java
@@ -2,8 +2,15 @@ package org.commonjava.event.file;
 
 import org.commonjava.event.common.IndyEvent;
 
-public class FileEvent extends IndyEvent
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class FileEvent extends IndyEvent implements Externalizable
 {
+
+    private static final int FILE_EVENT_VERSION = 1;
 
     private String trackingID;
 
@@ -27,4 +34,30 @@ public class FileEvent extends IndyEvent
     {
         this.trackingID = trackingID;
     }
+
+    public void writeExternal( ObjectOutput out ) throws IOException
+    {
+        super.writeExternal( out );
+
+        out.writeObject( Integer.toString( FILE_EVENT_VERSION ) );
+        out.writeObject( trackingID );
+
+    }
+
+    public void readExternal( ObjectInput in ) throws IOException, ClassNotFoundException
+    {
+        super.readExternal( in );
+
+        int version = Integer.parseInt( ( String )in.readObject() );
+        if ( version > FILE_EVENT_VERSION )
+        {
+            throw new IOException(
+                            "Cannot deserialize. This class is of an older version: " + FILE_EVENT_VERSION +
+                                            " vs. the version read from the data stream: " + version + "." );
+        }
+
+        this.trackingID = (String) in.readObject();
+
+    }
+
 }
